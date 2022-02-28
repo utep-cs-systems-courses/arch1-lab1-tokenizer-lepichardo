d2df6be87d24cd1550ee3c510f902c1f8afd858a
Luis_Pichardo <lepichardo@miners.utep.edu>
2022-02-28 09:15:12 -0700

    history.c updated, missing free_list

--- src/history.c
+++ src/history.c
@@ -2,24 +2,83 @@
 #include <stdlib.h>
 #include "history.h"
 
-struct litem{
-  struct litem *before, *after;
-  char *str;
-};
-struct litem head;
-
-
-
-
 List* init_history(){
-  head.before = head.after = &head;
-  return head;
+  // Creating space in the memory for the list
+  struct s_List *newList = (List*)malloc(sizeof(List));
+  newList->root = (Item*)malloc(sizeof(Item));
+  // Returning the start of the list
+  return newList;
 }
 
-void add_history(List *list, char *str){}
+void add_history(List *list, char *str){
+  // Using temp variable with new space in the memory
+  struct s_Item *tempItem = (Item*)malloc(sizeof(Item));
+  // Assigning tempItem to the head of the list
+  tempItem = (*list).root;
+  // int i to keep track of the id of the Item
+  int i = 0;
+  // while loop to iterarate through the list
+  while(tempItem->next != NULL){
+    // Using the next pointer
+    tempItem = tempItem->next;
+    // Incrementint i to keep the id updated
+    i++;
+  }
+  // Making more space in memory for the next Item
+  tempItem->next = (Item*)malloc(sizeof(Item));
+  // Assigning the value of str to the next item
+  tempItem->next->str = str;
+  // Assigning the id to the next item
+  tempItem->next->id = i;
+  // Update the null pointer
+  tempItem->next->next = NULL;
+}
 
-char *get_history(List *list, int id){}
+char *get_history(List *list, int id){
+  // Creating space for the temporary variable
+  struct s_Item *tempItem = (Item*)malloc(sizeof(Item));
+  // Assigning the temporary variable with the head of the list
+  tempItem = (*list).root;
+  // Iterating through the list 
+  while(tempItem != NULL){
+    // Checking if the id matches the id of each element
+    if(tempItem->id == id){
+      // Return the corrent element
+      return tempItem->str;
+    }
+    // Keep looking for the element with the id
+    tempItem = tempItem->next;
+  }
+  // Return \0 if id not found
+  return '\0';
+}
 
-void print_history(List *list){}
+void print_history(List *list){
+  // Creating space in memory for our temporaty variable
+  struct s_Item *tempItem = (Item*)malloc(sizeof(Item));
+  // Assigning the temporary variable with the head
+  tempItem = (*list).root;
+  // Checking if the head is empty
+  if(tempItem == NULL){
+    printf("Nothing to print...\n");
+    // Return since there is nothign to print
+    return;
+  }
+  // Checking if head has an item
+  if(tempItem->next == NULL){
+    printf("Only the head with no Items...\n");
+    // Return since there is nothing to print
+    return;
+  }
+  // Assigning the head with the first item of the list
+  tempItem = tempItem->next;
+  // Iterating through the list with the while loop
+  while(tempItem != NULL){
+    // Printing the current elelment of the list
+    printf("str: %s,\tid: %d\n",tempItem->str, tempItem->id);
+    // Checking the next element of the list
+    tempItem = tempItem->next;
+  }
+}
 
 void free_history(List *list){}
--- src/uimain.c
+++ src/uimain.c
@@ -16,7 +16,9 @@ void getString(){
 int main(void){
 
   char str[] = " hello\tworld,\nhave a good one! ";
-  
+
+  /*
+
   printf("\nMethods declared and initialized in tokenizer.c\n\n");
 
   // This is the space_char(char c) method
@@ -30,14 +32,18 @@ int main(void){
   // This is the *word_start(char *str) method
   printf("Method *word_start(char *str)\n");
   printf("This is the start of the word,'");
+  */
   char *start = word_start(str);
+  /*
   putchar(*start);
   printf("',end of the start\n\n");
 
   // This is the *word_terminator(char *word) method
   printf("Method *word_terminator(char *word)\n");
   printf("This is the word terminator,'");
+  */
   char *terminator = word_terminator(str);
+  /*
   putchar(*terminator);
   printf("',end of terminator\n\n");
 
@@ -49,14 +55,18 @@ int main(void){
   // This is the copy string(char *inStr, short len) method
   printf("Method *copy_str(char *inStr, short len)\n");
   printf("Printing the copy of the string...\n");
+  */
   char *newString = copy_str(str,sizeof(str));
+  /*
   printf(newString);
   printf("\nCopy of the string printed successfully!\n\n");
 
   // This is the **tokenize(char* str) method
   printf("Method **tokenize(char* str)\n");
   printf("Tokenizing the string...\n");
+  */
   char **tokens = tokenize(str);
+  /*
   printf("Tokenize done successfully!\n\n");
 
   // This is the print_tokens(char **tokens) method
@@ -68,10 +78,45 @@ int main(void){
   // This is the free_tokens(char **tokens) method
   printf("This is the free_tokens(char **tokens)\n");
   printf("Freeing tokens...\n");
+  */
   free_tokens(tokens);
+  /*
   printf("Tokens have been freed\n");
 
+  */
+    
+  printf("Methods in tokenize.c work fine\n");
+    
   printf("\nMethods declared and initialized in history.c\n\n");
+
+  // This is the init_history() method
+  printf("This is the init_history()\n");
+  printf("Creating new list...\n");
+  struct List *newList = init_history();
+  printf("New list created!\n\n");
+
+  // This is the add_history(List *list, char *str) method
+  printf("This is the add_history(List *list, char *str)\n");
+  printf("Adding new element to the list...\n\n");
+  add_history(newList, "hello");
+  add_history(newList, "world,");
+  add_history(newList, "have");
+  add_history(newList, "a");
+  add_history(newList, "good");
+  add_history(newList, "one!");
+
+  //This is the *get_history(List *list, int id) method
+  printf("This is the *get_history(List *list, int id)\n");
+  printf("Returning element at id 3...\n");
+  char *element = get_history(newList,3);
+  printf(element);
+  printf("\nDone\n\n");
+  // This is the print_history(List *list) method
+  printf("This is the print_history(List *list)\n");
+  printf("Printing list...\n");
+  print_history(newList);
+  printf("List printed!\n");
+
   printf("\n");
   
 }
